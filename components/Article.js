00e02bc441ee0a91c6e8734cd6d16243367b2430
John Youn <john@Johns-MacBook-Pro.local>
2020-09-02 21:08:52 -0700

    started menu

--- components/Article.js
+++ components/Article.js
@@ -1,8 +1,5 @@
 // This is the data we will be using to create our articles. Look at it, then proceed to line 93.
 // OPTIONAL: if you're feeling adventurous, try to make this data an export from a different module, and import it here.
-
-const ParcelBundler = require("parcel-bundler");
-
 // You can read about ES6 modules here: https://exploringjs.com/es6/ch_modules.html#sec_basics-of-es6-modules
 const data = [
   {
@@ -96,24 +93,17 @@ const data = [
   Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
   and returns a DOM node looking like the one below:
-
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
-
     {three separate paragraph elements}
-
     <span class="expandButton">+</span>
   </div>
-
   Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
   This listener should toggle the class 'article-open' on div.article.
-
   Step 3: Don't forget to return something from your function!
-
   Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
   to create a div.article element and append it to the DOM inside div.articles (see index.html).
-
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
@@ -124,8 +114,8 @@ function articleMaker(title, date, para, para2, para3) {
   const articleTitle = document.createElement("h2");
   const articleDate = document.createElement("p");
   const p1 = document.createElement("p");
-  const p2 = document.createElement("p2");
-  const p3 = document.createElement("p3");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
   const button = document.createElement("span");
 
   //structure / append
@@ -153,7 +143,7 @@ function articleMaker(title, date, para, para2, para3) {
   button.textContent = "+";
 
   button.addEventListener("click", () => {
-    container.classList.toggle("container-open");
+    container.classList.toggle("article-open");
   });
 
   return container;
@@ -163,10 +153,15 @@ data.forEach((item) => {
   const artData = articleMaker(
     item.title,
     item.date,
-    item.para,
-    item.para1,
-    item.para2,
-    item.para3
+    item.firstParagraph,
+    item.secondParagraph,
+    item.thirdParagraph,
+    item.firstParagraph,
+    item.secondParagraph,
+    item.thirdParagraph,
+    item.firstParagraph,
+    item.secondParagraph,
+    item.thirdParagraph
   );
   articles.appendChild(artData);
 });
--- components/Menu.js
+++ components/Menu.js
@@ -1,12 +1,12 @@
 // This is the data we will be using, study it but don't change anything, yet.
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out",
 ];
 
 /* 
@@ -31,3 +31,26 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+function menuMaker(items) {
+  const menu = document.createElement("div");
+  const list = document.createElement("ul");
+
+  menu.classList.add(menu);
+  menu.appendChild(list);
+
+  menuStuff.forEach((item) => {
+    const itemArr = document.createElement("li");
+    itemArr.textContent = item;
+    list.appendChild(itemArr);
+  });
+
+  const menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+  });
+  return menu;
+}
+
+const menuHead = document.querySelector("header");
+menuHead.appendChild(menuMaker(menuStuff));
